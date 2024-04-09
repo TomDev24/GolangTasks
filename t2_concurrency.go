bc9918b4a07f259ac8a89c24c4f907ce15ffe528
TomDev24 <mail.com67747@gmail.com>
2024-04-09 14:39:50 +0300

    t2 is done

--- t2_concurrency.go
+++ t2_concurrency.go
@@ -3,27 +3,57 @@ package main
 import (
 	"fmt"
 	"sync"
+	"log"
+	"os"
 )
 
-func powerOfTwo(num int){
-	fmt.Printf("%d ", num * num)
+func powerOfTwoLog(num int, logger *log.Logger){
+	//fmt.Printf("%d ", num * num) // We cannot use fmt lib, cause its not safe in concurrent environment
+	logger.Printf("%d", num * num) // logger is safe to use, cause it locks mutex when printing
 }
 
-// Solution using WaitGroups
-func main(){
-	var wg sync.WaitGroup
+func powerOfTwoMutex(num int, m *sync.Mutex){
+	m.Lock()
+	defer m.Unlock()
+	fmt.Printf("%d\n", num * num)
+}
+
+
+// Solution using logger
+func useLogger() {
+	logger := log.New(os.Stdout, "", 0) // using STDOUT instead of STDERR
+	wg := sync.WaitGroup{}
 	nums := []int{2,4,6,8,10}
 
 	wg.Add(len(nums))
 	for _, n := range nums {
 		go func(num int){
 			defer wg.Done()
-			powerOfTwo(num)
+			powerOfTwoLog(num, logger)
 		}(n)
 	}
-
 	wg.Wait()
-	fmt.Println()
 }
 
+func useMutex() {
+	m := sync.Mutex{}
+	wg := sync.WaitGroup{}
+	nums := []int{2,4,6,8,10}
+
+	wg.Add(len(nums))
+	for _, n := range nums {
+		go func(num int){
+			defer wg.Done()
+			powerOfTwoMutex(num, &m) // mutexes must not be copied,
+		}(n)
+	}
+	wg.Wait()
+}
+
+func main(){
+	fmt.Println("Using logger:")
+	useLogger()
+	fmt.Println("Using mutex:")
+	useMutex()
+}
 
